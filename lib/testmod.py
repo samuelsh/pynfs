0feea56ad6b758b64307e03474dcb981afd263f5
samuel <samuel@vastdata.com>
2019-06-12 19:41:47 +0300

    Remove tests MKNOD1, CREATE7 from failures list, since passing on CentOS NFS server

--- lib/testmod.py
+++ lib/testmod.py
@@ -526,7 +526,6 @@ def printresults(tests, opts, file=None):
         TEMP_FAILURES += ['LKU1m_MULTICONN', 'LOCK1m_MULTICONN']
 
     #Appending nfsv3 expected failures
-    # CREATE7: Fails unless vfs.nfsrv.create_attributes_ids_enabled = 1
     TEMP_FAILURES.extend(['MOUNT_MNT_V1', 'MOUNT_DUMP_V1'])
 
     TEMP_WARNINGS = ['LOCK8c', 'MKBLK', 'MKCHAR', 'RD12', 'SATT2c',
